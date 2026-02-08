29b1c139bc4f13eb91abdad319032912e0b51bf0
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-08 23:37:34 +0400

    solve HackerRank 30 Days of Code - Day 2

---
+++ src/thirtyDaysofCode/day2/Day2.java
@@ -0,0 +1,17 @@
+package thirtyDaysofCode.day2;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Day2 {
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println(i+" "+d+" "+s);
+        scan.close();
+    }
+}
