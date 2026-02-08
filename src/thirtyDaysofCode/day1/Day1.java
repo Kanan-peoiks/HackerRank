cf42a6c41a628ee5735aee273cea289872ccd674
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-02-08 23:37:25 +0400

    solve HackerRank 30 Days of Code - Day 1

---
+++ src/thirtyDaysofCode/day1/Day1.java
@@ -0,0 +1,15 @@
+package thirtyDaysofCode.day1;
+
+import java.util.Scanner;
+
+public class Day1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String inputString =scan.nextLine();
+
+        scan.close();
+
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+    }
+}
